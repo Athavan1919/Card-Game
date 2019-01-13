162839de087d944b14c9c49e581b35783f6ca86d
Aman Riat <amanriat777@gmail.com>
2019-01-13 13:37:35 -0500

    Finished A1Q3 and tested

--- 01/A1Q3.java
+++ 01/A1Q3.java
@@ -1,6 +1,43 @@
 public class A1Q3{
 	
-	public static void main (String[] args){
-	
+	public static int getLongestRun(double[] list){
+		if (list.length <= 0){
+			return 0;
+		}
+		int index = 0;
+		int longestRun = 1;
+		while (index < list.length -1){
+			int run = 1;
+			while ((index < list.length -1) && (list[index] == list[index +1])){
+				run += 1;
+				index += 1;
+			}
+			if (run > longestRun){
+				longestRun = run; 
+			}
+			index += 1;
+		}
+		return longestRun;
 	}
+
+	public static int getLongestRun(String[] list){
+		if (list.length <= 0){
+			return 0;
+		}
+		int index = 0;
+		int longestRun = 1;
+		while (index < list.length -1){
+			int run = 1;
+			while ((index < list.length -1) && (list[index].equals(list[index+1]))){
+				run += 1;
+				index += 1; 
+			}
+			if (run > longestRun){
+				longestRun = run; 
+			}
+			index += 1;
+		}
+		return longestRun;
+	}
+
 }
